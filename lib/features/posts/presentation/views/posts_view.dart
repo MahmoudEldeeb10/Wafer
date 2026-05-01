a9c87bdb6788c0ea83da2a1e8b061bdb276fc446
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-05-01 09:57:14 +0300

    edit posts view

--- lib/features/posts/presentation/views/posts_view.dart
+++ lib/features/posts/presentation/views/posts_view.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wafer/core/utils/app_colors.dart';
-import 'package:wafer/core/utils/styles.dart';
 import 'package:wafer/core/widgets/custom_app_bar.dart';
-import 'package:wafer/features/posts/presentation/views/widgets/custom_card.dart';
 
 class PostsView extends StatelessWidget {
   const PostsView({super.key});
