c1fcb08df3346c8ce99e7c87028132abc459d9fc
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-05-20 00:27:13 +0300

    edit custom appbar

--- lib/core/widgets/custom_app_bar.dart
+++ lib/core/widgets/custom_app_bar.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:wafer/core/utils/app_colors.dart';
 import 'package:wafer/core/utils/styles.dart';
 
 class CustomAppBar extends StatelessWidget {
   final String title;
   final IconData firstIcon;
   final IconData secondIcon;
+  final VoidCallback? onFirstIconTap; // أضف الـ callback
+  final VoidCallback? onSecondIconTap;
+
   const CustomAppBar({
     super.key,
     required this.title,
     required this.firstIcon,
     required this.secondIcon,
+    this.onFirstIconTap,
+    this.onSecondIconTap,
   });
 
   @override
@@ -23,9 +27,23 @@ class CustomAppBar extends StatelessWidget {
           style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
         ),
         Spacer(),
-        Icon(firstIcon, size: 28, color: Colors.black.withOpacity(0.7)),
+        GestureDetector(
+          onTap: onFirstIconTap,
+          child: Icon(
+            firstIcon,
+            size: 28,
+            color: Colors.black.withOpacity(0.7),
+          ),
+        ),
         SizedBox(width: 8),
-        Icon(secondIcon, size: 28, color: Colors.black.withOpacity(0.7)),
+        GestureDetector(
+          onTap: onSecondIconTap,
+          child: Icon(
+            secondIcon,
+            size: 28,
+            color: Colors.black.withOpacity(0.7),
+          ),
+        ),
       ],
     );
   }
